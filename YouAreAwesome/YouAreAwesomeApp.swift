f779ff3630da5789c1314c312877e6dc437cac8f
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-23 04:20:27 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Lori Rothermel on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
