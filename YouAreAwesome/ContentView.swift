26e45a85df1f7bd4addd9229d41ba810bf204b9c
lorirothermel <lori@rothermel1.com>
2024-08-19 22:13:49 -0400

    Getting Layout Right - Part 1

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .border(.orange, width: 1)
+                .padding()
+            
+            HStack {
+            
+                Button("Awesome") {
+                    // Action performed when button is pressed.
+                    messageString = "You Are Awesome!"
+                    
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    // Action performed when button is pressed.
+                    messageString = "You Are Great!"
+                    
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                                
+                .buttonStyle(.borderedProminent)
+                
+                
+            }  // HStack
+            
+            .border(.black, width: 5)
+   
+            
+        }  // VStack
+           
+                             
+            
+        
+        
+        
+    }  // some View
+}  // View
 
 #Preview {
     ContentView()
