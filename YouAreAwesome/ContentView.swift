798a562e730f382f93e163bfa2bf702e80054744
lorirothermel <lori@rothermel1.com>
2024-08-20 21:01:04 -0400

    Group

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,69 +13,77 @@ struct ContentView: View {
     
     
     var body: some View {
-                  
-        GeometryReader { geo in
-            
-            VStack {
-                               
-                    Spacer()
-                    
-                    Text(messageString)
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.red)
-                        .frame(height: 150)
-                        .frame(maxWidth: .infinity, alignment: .center)
-         //               .border(.orange, width: 1)
-                        .padding()
-                    
-                    Spacer()
-                        
-                    Divider()
-                        .background(.red)
-                        .padding()
-                        .frame(width: 350.0)
-                        
-                    Rectangle()
-                        .fill(.indigo)
-                        .frame(width: geo.size.width * (2/3), height: 3)
-                    
-                    
-                    HStack {
-                    
-                        Button("Awesome") {
-                            // Action performed when button is pressed.
-                            messageString = "You Are Awesome!"
-                            
-                        }  // Button
-                        .buttonStyle(.borderedProminent)
-                        
-                        Spacer()
-                                            
-                        Button("Great") {
-                            // Action performed when button is pressed.
-                            messageString = "You Are Great!"
-                            
-                        }  // Button
-                        .buttonStyle(.borderedProminent)
-                                        
-                        .buttonStyle(.borderedProminent)
-                        
-                        
-                    }  // HStack
-                    
-        //            .border(.black, width: 5)
-                    .padding()
-                    
-            }  // VStack
-            
-        }  // GeoReader
-            
-
         
-  
+        VStack {
+            
+            Group {
+                Text("I'm am a Text View #1")
+                Text("I'm am a Text View #2")
+                Text("I'm am a Text View #3")
+                Text("I'm am a Text View #4")
+                Text("I'm am a Text View #5")
+                Text("I'm am a Text View #6")
+                Text("I'm am a Text View #7")
+                Text("I'm am a Text View #8")
+                Text("I'm am a Text View #9")
+                Text("I'm am a Text View #10")
+                Text("I'm am a Text View #11")
+                Text("I'm am a Text View #12")
+                Text("I'm am a Text View #13")
+                Text("I'm am a Text View #14")
+                Text("I'm am a Text View #15")
+                Text("I'm am a Text View #16")
+                Text("I'm am a Text View #17")
+                Text("I'm am a Text View #18")
+                Text("I'm am a Text View #19")
+                Text("I'm am a Text View #20")
+                   
+            }  // Group
+            .font(.title)
+            .fontWeight(.heavy)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity, alignment: .center)
+            //               .border(.orange, width: 1)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                
+                Button("Awesome") {
+                    // Action performed when button is pressed.
+                    messageString = "You Are Awesome!"
+                    
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    // Action performed when button is pressed.
+                    messageString = "You Are Great!"
+                    
+                }  // Button
+                .buttonStyle(.borderedProminent)
+                
+                .buttonStyle(.borderedProminent)
+                
+                
+            }  // HStack
+            
+            //            .border(.black, width: 5)
+            .padding()
+            
+        }  // VStack
         
     }  // some View
 }  // View
